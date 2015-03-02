e2f2672481390c027575080cd43ddab4072d0021
1309013 <1309013@RG41107.rgu.ac.uk>
2015-03-02 10:17:21 +0000

    ShopOrder added

---
+++ JavaApplication17/src/javaapplication17/ShopOrder.java
@@ -0,0 +1,16 @@
+package javaapplication17;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author 1309013
+ */
+public class ShopOrder extends Order {
+ 
+
+    
+}
